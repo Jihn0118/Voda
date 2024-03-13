a1a560f24da348bb2241d1584e3044b2383e20d4
전소민 <goflvhxj96@naver.com>
2024-03-13 12:40:25 +0900

    feaet: init 메서드 작성

--- backend/src/main/java/io/watssuggang/voda/diary/dto/DtoExample.java
+++ backend/src/main/java/io/watssuggang/voda/diary/dto/DtoExample.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:85348ae67c430337e65e840ba153482f1daabdf8f9e9b00518a15cc9deebaaaf
-size 95
+package io.watssuggang.voda.diary.dto;
+
+import lombok.Data;
+
+@Data
+public class DtoExample {
+}
