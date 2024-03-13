4905aced5eb3e5c7b8834586265511b6e182d40c
zqwz <zqwz@naver.com>
2024-03-13 13:06:34 +0900

    feat: 펫 레벨업 기능 추가

--- backend/src/main/java/io/watssuggang/voda/pet/dto/res/PetResponse.java
+++ backend/src/main/java/io/watssuggang/voda/pet/dto/res/PetResponse.java
@@ -1,33 +1,3 @@
-package io.watssuggang.voda.pet.dto.res;
-
-import io.watssuggang.voda.common.enums.Emotion;
-import io.watssuggang.voda.common.util.DateUtil;
-import io.watssuggang.voda.pet.domain.Pet;
-import io.watssuggang.voda.pet.domain.PetFile;
-import lombok.Getter;
-
-@Getter
-public class PetResponse {
-
-    private final String name;
-    private final PetFileResponse petImgUrl;
-    private final Byte exp;
-    private final Byte level;
-    private final Emotion emotion;
-    private final Byte stage;
-    private final Boolean isFeed;
-
-    private PetResponse(Pet pet, PetFile petFile) {
-        this.name = pet.getPetName();
-        this.petImgUrl = PetFileResponse.of(petFile);
-        this.exp = pet.getPetExp();
-        this.level = pet.getPetLevel();
-        this.emotion = pet.getPetEmotion();
-        this.stage = pet.getPetStage();
-        this.isFeed = DateUtil.AfterTodayMidNight(pet.getPetLastFeed());
-    }
-
-    public static PetResponse of(Pet pet, PetFile petFile) {
-        return new PetResponse(pet, petFile);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f29ea6d996a9677b0fea10e33bffd641c69ca112f34cd7669d8ff99a56a7f6e
+size 1169
