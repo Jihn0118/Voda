2b5c25c8b6bf0a2692170b5e69e53e05f13fe7a5
이수민 <suminjeff@gmail.com>
2024-03-13 10:12:11 +0900

    feat(login): Modify buttons

--- frontend/voda/src/components/diary/DiaryList.jsx
+++ frontend/voda/src/components/diary/DiaryList.jsx
@@ -1,7 +1,3 @@
-import React from "react";
-
-const DiaryList = () => {
-  return <div>DiaryList</div>;
-};
-
-export default DiaryList;
+version https://git-lfs.github.com/spec/v1
+oid sha256:ceefcf199ac288be332ba601c1cbc17daaaf532bd0c8e261365ee0df454e7386
+size 115
