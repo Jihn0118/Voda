9402d1de8ae2ca7be6e26952986b6a4cfcefa33e
김병주 <qudwn2941@naver.com>
2024-03-13 13:35:41 +0900

    Feat: filter box diary list
    Related to: S10P22A104-103

--- frontend/voda/src/pages/diarylist/DiaryList.jsx
+++ frontend/voda/src/pages/diarylist/DiaryList.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-const DiaryList = () => {
-  return <div>DiaryList</div>;
-};
-
-export default DiaryList;
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e48a7f7edfc426016176dbea87c21a3438ca2befc037ae92ded8def57a930af
+size 569
