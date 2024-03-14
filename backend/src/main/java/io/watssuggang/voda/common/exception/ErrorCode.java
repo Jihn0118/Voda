b756729e21990b958e2c30b9f11c54ef78ac6a11
zqwz <zqwz@naver.com>
2024-03-14 12:41:04 +0900

    feat: 아이템 생성 수정 예외처리

--- backend/src/main/java/io/watssuggang/voda/common/exception/ErrorCode.java
+++ backend/src/main/java/io/watssuggang/voda/common/exception/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23d53b250bd2e5550d75cf9fd286ed8706e9ebfd18b717866376f58bb7807158
-size 346
+oid sha256:24a81b97c270e4d92abe9843b5f0bba86e3fa4cfb0ac857ce354b5c7741a4adf
+size 520
