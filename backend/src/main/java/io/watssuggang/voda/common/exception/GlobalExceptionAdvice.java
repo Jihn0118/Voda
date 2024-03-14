63d19b0eb9dab43be999b8c97b2a82b86e47e3d4
zqwz <zqwz@naver.com>
2024-03-14 11:13:44 +0900

    feat: MethodArgumentNotValidException 핸들러, response 추가

--- backend/src/main/java/io/watssuggang/voda/common/exception/GlobalExceptionAdvice.java
+++ backend/src/main/java/io/watssuggang/voda/common/exception/GlobalExceptionAdvice.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:770c484535bafa6da4ed7f8078feefe8fa3b328a72367eee6a3a8acd2c7eeec1
-size 220
+oid sha256:b99e19e262138a790cbbcabcf9d3c8fbdf911111d4e86f4799a71090a47836f6
+size 1052
