f13456884acf412c330c083deb2a53aeb3f9d60d
zqwz <zqwz@naver.com>
2024-03-14 13:51:50 +0900

    feat: 상점용 모든 아이템 조회 API 구현

--- backend/src/main/java/io/watssuggang/voda/pet/controller/ItemController.java
+++ backend/src/main/java/io/watssuggang/voda/pet/controller/ItemController.java
@@ -1,34 +1,3 @@
-package io.watssuggang.voda.pet.controller;
-
-import io.watssuggang.voda.pet.dto.req.ItemRequest;
-import io.watssuggang.voda.pet.dto.req.ItemUpdateRequest;
-import io.watssuggang.voda.pet.dto.res.ItemResponse;
-import io.watssuggang.voda.pet.service.ItemService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/item")
-public class ItemController {
-
-    private final ItemService itemService;
-
-    @PostMapping
-    public ResponseEntity<ItemResponse> createItem(
-            @RequestBody @Valid ItemRequest postRequest
-    ) {
-        ItemResponse item = itemService.createItem(postRequest);
-        return ResponseEntity.ok(item);
-    }
-
-    @PatchMapping("{item-id}")
-    public ResponseEntity<?> createItem(
-            @RequestBody @Valid ItemUpdateRequest updateRequest,
-            @PathVariable("item-id") Integer itemId) {
-        ItemResponse itemResponse = itemService.updateItem(itemId, updateRequest);
-        return ResponseEntity.ok(itemResponse);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:63b5b1cc1b8620f47946a757d2a05396d99a619a4211c106c825091c11429a4a
+size 1390
--- backend/src/main/java/io/watssuggang/voda/pet/dto/res/ItemResponse.java
+++ backend/src/main/java/io/watssuggang/voda/pet/dto/res/ItemResponse.java
@@ -1,24 +1,3 @@
-package io.watssuggang.voda.pet.dto.res;
-
-import io.watssuggang.voda.pet.domain.Item;
-import lombok.Getter;
-
-@Getter
-public class ItemResponse {
-
-    private final Integer itemId;
-    private final String imgURl;
-    private final Integer price;
-    private final String name;
-
-    private ItemResponse(Item item) {
-        this.itemId = item.getItemId();
-        this.imgURl = item.getItemImageUrl();
-        this.price = item.getItemPrice();
-        this.name = item.getItemName();
-    }
-
-    public static ItemResponse of(Item item){
-        return new ItemResponse(item);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa089588a85a2ae2548f5d339aa6ccbadf6988fe0522429618b60e15e5bd1286
+size 735
