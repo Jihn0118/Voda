52e7b49a201634726a8698a2082f098320d44776
zqwz <zqwz@naver.com>
2024-03-14 11:12:41 +0900

    feat: 예외 핸들러, 예외 처리 클래스 정의

---
+++ backend/src/main/java/io/watssuggang/voda/common/exception/ErrorCode.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:23d53b250bd2e5550d75cf9fd286ed8706e9ebfd18b717866376f58bb7807158
+size 346
---
+++ backend/src/main/java/io/watssuggang/voda/common/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:770c484535bafa6da4ed7f8078feefe8fa3b328a72367eee6a3a8acd2c7eeec1
+size 220
