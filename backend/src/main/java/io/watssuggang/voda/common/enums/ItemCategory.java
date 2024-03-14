bf38ba14bc2ee70e83b346f9df313973f66a750b
zqwz <zqwz@naver.com>
2024-03-14 13:50:15 +0900

    feat: 아이템 코드 컨버터 등록

--- backend/src/main/java/io/watssuggang/voda/common/enums/ItemCategory.java
+++ backend/src/main/java/io/watssuggang/voda/common/enums/ItemCategory.java
@@ -1,6 +1,3 @@
-package io.watssuggang.voda.common.enums;
-
-public enum ItemCategory {
-
-    FOOD, EFFECT;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:eeea346b306350a389f10766f9400b90b5c3f31bceadbd0ee1dca9a82cb66e3b
+size 552
