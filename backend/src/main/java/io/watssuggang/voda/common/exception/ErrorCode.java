4f791dfd063387af25fa3f21c4e5d8175c039318
김진호 <lantern50@naver.com>
2024-03-15 09:59:16 +0900

    Refactor: Auth, Member exception 수정

--- backend/src/main/java/io/watssuggang/voda/common/exception/ErrorCode.java
+++ backend/src/main/java/io/watssuggang/voda/common/exception/ErrorCode.java
@@ -1,19 +1,3 @@
-package io.watssuggang.voda.common.exception;
-
-import lombok.Getter;
-import org.springframework.http.HttpStatus;
-
-@Getter
-public enum ErrorCode {
-
-    DUPLICATE_ITEM_NAME(HttpStatus.CONFLICT, "이미 존재하는 아이템 이름입니다."),
-    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "아이템을 찾을 수 없습니다.");
-
-    private final HttpStatus status;
-    private final String message;
-
-    ErrorCode(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4dcffdf1018a6d6d0f830997a5ad52ac4a7b6551fa5059a8b1fd8028d471de6
+size 678
