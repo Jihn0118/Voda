30baf9d2078838420634765e52e187ce4c43a6d4
이수민 <suminjeff@gmail.com>
2024-03-20 13:55:29 +0900

    feat(front): Fix login bugs

--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7f181d26655bda1a4c3604618f32057421c297057408dda2e325a8cc64d630c
-size 2379
+oid sha256:cb1fa5f5af2906027787de68f1f80f7cc639af26750feb4f92351a83d6c05e44
+size 2387
