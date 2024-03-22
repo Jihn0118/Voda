fea6310fe79faaf3ed796b7914f39580c9e21ec7
김진호 <lantern50@naver.com>
2024-03-22 15:50:56 +0900

    Feat: 다이어리 예외처리

--- backend/src/main/java/io/watssuggang/voda/member/service/MemberService.java
+++ backend/src/main/java/io/watssuggang/voda/member/service/MemberService.java
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:21ce9f50759b5fc9f7cb51d0756e57e4026edf41eaad7307fb284336030779e1
-size 274
+package io.watssuggang.voda.member.service;
+
+import io.watssuggang.voda.member.domain.Member;
+import io.watssuggang.voda.member.dto.req.SignUpRequest;
+
+public interface MemberService {
+
+    Member findByEmail(String uid);
+
+    Integer signUp(SignUpRequest signUpRequest);
+}
