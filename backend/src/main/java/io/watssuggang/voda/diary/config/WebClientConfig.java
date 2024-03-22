d66f33e2acbc103e3f99194c37265dddfab10be9
전소민 <goflvhxj96@naver.com>
2024-03-22 10:04:35 +0900

    transfer

--- backend/src/main/java/io/watssuggang/voda/diary/config/WebClientConfig.java
+++ backend/src/main/java/io/watssuggang/voda/diary/config/WebClientConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9c232a7c85f044ac2f325247acacb9f1b338131a04612e1699e3e14f33b59a6
-size 2385
+oid sha256:90d283f4b02ff5b48c120129a0e67e0e8610f47c56e1c615420d36e1209f57ed
+size 3565
--- backend/src/main/java/io/watssuggang/voda/diary/util/PromptHolder.java
+++ backend/src/main/java/io/watssuggang/voda/diary/util/PromptHolder.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9130aa3fc1f4bf71f80fcc28e96d54bc059c69dea4162112c764b196ebe9b011
-size 624
+oid sha256:252f1ef603fb72770d62e4ffecc9dc86924d4291c7041195963d155fac317da6
+size 831
