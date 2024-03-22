4f65e832c45b3ed44046e04f95cacd28cd5dd54d
전소민 <goflvhxj96@naver.com>
2024-03-22 12:46:11 +0900

    Fix: 누락된 내용 추가

--- backend/src/main/java/io/watssuggang/voda/common/security/config/SecurityConfig.java
+++ backend/src/main/java/io/watssuggang/voda/common/security/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:051ab506fb0a57f1f0b90b99cfe70ea5d62c3144c1bef10f89b7680d661ae6e8
-size 5726
+oid sha256:4224dbbc31bca78a88fadd454b894fb42122eac853ef8a4ce3d62385813807ba
+size 5656
