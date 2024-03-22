74ddb7c8b05e091b3c85cca3a692ca19f0fe6ac8
zqwz <zqwz@naver.com>
2024-03-22 14:44:33 +0900

    fix: Karlo 요청 dto 수정

--- backend/src/main/java/io/watssuggang/voda/diary/dto/req/KarloRequest.java
+++ backend/src/main/java/io/watssuggang/voda/diary/dto/req/KarloRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c916e125253f59043ed3efab36de0363aa4f24c7cdec9f61eda2a0119d64e3b
-size 2098
+oid sha256:d3f30b62be23d9d7e24a8650ebcf75940a51dd31a4621d971d4b82a4dfb75e42
+size 2752
