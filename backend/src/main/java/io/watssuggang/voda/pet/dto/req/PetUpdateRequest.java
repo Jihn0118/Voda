879e3dfee35ea49ef4f253fbdbcf297ca89a1c18
zqwz <zqwz@naver.com>
2024-03-25 16:10:51 +0900

    fix: 펫 API 권한 및 유효성 검사 추가

--- backend/src/main/java/io/watssuggang/voda/pet/dto/req/PetUpdateRequest.java
+++ backend/src/main/java/io/watssuggang/voda/pet/dto/req/PetUpdateRequest.java
@@ -1,12 +1,3 @@
-package io.watssuggang.voda.pet.dto.req;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class PetUpdateRequest {
-
-    private String name;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:41fa3128b848a8774f8253806fccea73e9f23285a18bd5497fd0d0d65eb6c955
+size 568
