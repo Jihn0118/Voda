c70a6605860205d04eefe89b85ebb6d70c2eb01a
이수민 <suminjeff@gmail.com>
2024-03-27 16:30:22 +0900

    feat(pet): Fix pet feed bug

--- frontend/voda/src/services/pet.js
+++ frontend/voda/src/services/pet.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41fe3d600c156105be7629db0d7dd972190b9b6918a40636a9c9ac03fd006338
-size 1176
+oid sha256:85ae91faddbc237c5bca69fd5aaaa850185c8aa279ab5674373fcc4a7060bb06
+size 1175
