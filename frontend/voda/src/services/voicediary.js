106d0ad0c290f0efd419401b479d161b2498a4f1
김병주 <qudwn2941@naver.com>
2024-03-27 14:52:54 +0900

    Feat: modify chatting diary create

--- frontend/voda/src/services/voicediary.js
+++ frontend/voda/src/services/voicediary.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2fa313b16824b8bdf72d1027732d4213fbbe93527faec53289a7fffdbe48c3f
-size 1764
+oid sha256:fae3a55f24bd59ef10ba222df8199a98b0cc568f71b6d7f2eede10bca26b4e4d
+size 1754
