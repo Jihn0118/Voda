3a617acb3fb70afd99246df2c519af74e5a7efd3
전소민 <goflvhxj96@naver.com>
2024-03-27 14:23:28 +0900

    feat: diary file save 추가

--- backend/src/main/java/io/watssuggang/voda/diary/domain/Diary.java
+++ backend/src/main/java/io/watssuggang/voda/diary/domain/Diary.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7f7e0eaf1990d6eb146b1c1ebb7572b2c53789de3971c176fc23981fb0af09c
-size 2062
+oid sha256:327afc98974a55e91049fa25e20c94377c49b3c263fd2d8de09ba57a10a60fe9
+size 2057
