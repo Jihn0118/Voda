29e8188adc548800bb0d63ed5611a93b24bb8569
김병주 <qudwn2941@naver.com>
2024-03-28 12:55:34 +0900

    Fix: diary audio file play & pet name change

--- frontend/voda/src/components/pet/Pet.jsx
+++ frontend/voda/src/components/pet/Pet.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61aa40f9a496c08434374d7c336e02d67233f98f6208304df7ea9ac880feb561
-size 4103
+oid sha256:ab2028e6022458801adcb88a8661b47f6b5fd7f9b734c9327beebc5322bbb609
+size 4147
