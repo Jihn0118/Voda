254f7310b6b3cc2d33d660ed13547f19dec777b8
zqwz <zqwz@naver.com>
2024-03-28 16:28:48 +0900

    feat: 포인트 로그 로직 추가

--- backend/src/main/java/io/watssuggang/voda/diary/service/FileUploadService.java
+++ backend/src/main/java/io/watssuggang/voda/diary/service/FileUploadService.java
@@ -1,3 +1,46 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:364a0b145a606d2aa4f5f94b17193a38d584d8b003c9e02b5ac51a45ec0f6a8b
-size 1807
+package io.watssuggang.voda.diary.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import jakarta.transaction.Transactional;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.util.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class FileUploadService {
+
+    private final AmazonS3Client amazonS3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    String fileUpload(Integer userId, String contentType, String folder, String fileType,
+            MultipartFile multipartFile)
+            throws IOException {
+        return fileUpload(userId, contentType, folder, fileType, multipartFile.getBytes());
+    }
+
+    String fileUpload(Integer userId, String contentType, String folder, String fileType,
+            byte[] byteArray) {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        String filePath = folder + "/" + uuid + "." + fileType;
+        Map<String, String> userInfo = new HashMap<>();
+        userInfo.put("userId", String.valueOf(userId));
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(contentType);
+        metadata.setContentLength(byteArray.length);
+        metadata.setLastModified(Date.from(ZonedDateTime.now().toInstant()));
+        metadata.setUserMetadata(userInfo);
+        amazonS3Client.putObject(bucket, filePath, new ByteArrayInputStream(byteArray),
+                metadata);
+        return amazonS3Client.getUrl(bucket, filePath).toString();
+    }
+
+}
