476462e0ca8403043ef51fee27d5b381e9dbf1bf
김병주 <qudwn2941@naver.com>
2024-03-29 11:21:41 +0900

    Fix: editAllow logic update

--- frontend/voda/src/main.jsx
+++ frontend/voda/src/main.jsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2a9fb2c586c3c785dee6c33b8a8d5e8478e8f462d50de6059b23abbd0b0fad11
-size 331
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
+);
