5823d42c3e9614718f7772dd9e5dabe18cba743a
김병주 <qudwn2941@naver.com>
2024-04-01 15:42:54 +0900

    Fix: pling sound issue

--- frontend/voda/src/components/pet/ExpBar.jsx
+++ frontend/voda/src/components/pet/ExpBar.jsx
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b9d7005ed79b6a5034091a1f5a9e978bbce8cdf240319a684f03f47cd39d91bc
-size 978
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { usePetStore } from "../../store/petStore";
+
+const Bar = styled.div`
+  background-color: #686868;
+  border-radius: 1rem;
+  height: 1rem;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const ExpBarFill = styled.div`
+  height: 85%;
+  width: ${({ exp }) => `${exp}%`};
+  background-color: #b8eea4;
+  border-radius: 1rem;
+  transition: width 0.5s ease-in-out; /* 경험치가 변경될 때 애니메이션 효과 추가 */
+`;
+
+export default function ExpBar() {
+  const { level, exp } = usePetStore();
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "0.5rem",
+      }}
+    >
+      <h3 style={{ margin: "0" }}>Lv.{level}</h3>
+      <Bar>
+        <ExpBarFill exp={exp * 10} />
+      </Bar>
+    </div>
+  );
+}
