1c25b2238caa12a53b6a2009016a6556ff67b26f
이수민 <suminjeff@gmail.com>
2024-04-01 16:05:50 +0900

    fix: Fix emoji url

--- frontend/voda/src/components/user/LoadingReport.jsx
+++ frontend/voda/src/components/user/LoadingReport.jsx
@@ -1,26 +1,3 @@
-import React from "react";
-import styled from "styled-components";
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 30% auto;
-`;
-const LoadingReport = () => {
-  const month = new Date().getMonth() + 1;
-  const EMOJI_URL = import.meta.env.VITE_EMOJI_URL;
-  return (
-    <Container>
-      <div style={{ fontWeight: "bold", fontSize: "1.5rem", padding: "10%" }}>
-        {month}월 일기 분석중 ...
-      </div>
-      <img
-        src={`${EMOJI_URL}/People%20with%20professions/Woman%20Technologist%20Light%20Skin%20Tone.png`}
-        alt="Technologist"
-        width="300"
-        height="300"
-      />
-    </Container>
-  );
-};
-export default LoadingReport;
+version https://git-lfs.github.com/spec/v1
+oid sha256:94c3b29d6d91d9089565638dec0361a3aa174483c5626efb17f746c7b1ca1a19
+size 717
