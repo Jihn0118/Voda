4928865c028874de4798a992b7fd666e0bc56033
이수민 <suminjeff@gmail.com>
2024-04-01 15:31:33 +0900

    fix: Fix emoji url

--- frontend/voda/src/store/store.js
+++ frontend/voda/src/store/store.js
@@ -1,29 +1,3 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware"; // 디버깅 용도
-
-const store = (set, get) => ({
-  emotions: {
-    ANGER:
-      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Enraged%20Face.png",
-    JOY: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Grinning%20Face%20with%20Smiling%20Eyes.png",
-    SADNESS:
-      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Loudly%20Crying%20Face.png",
-    FEAR: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Fearful%20Face.png",
-    CURIOSITY:
-      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Nerd%20Face.png",
-  },
-  editAllow: false,
-  changeEditMode: () => {
-    set((state) => ({ editAllow: !state.editAllow }));
-  },
-  diaryId: "",
-  setDiaryId: (value) => {
-    set({ diaryId: value });
-  },
-});
-
-const useStore = create(
-  process.env.NODE_ENV !== "production" ? devtools(store) : store
-);
-
-export default useStore;
+version https://git-lfs.github.com/spec/v1
+oid sha256:dba4e99408dbcc7a16c8640d9f056ac1f75eca65fa2c246b2599f8ff6dba7b18
+size 697
