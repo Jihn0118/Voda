67a91848aaac14071dee60ddb5c7eea942f9b1f6
김병주 <qudwn2941@naver.com>
2024-04-02 10:19:08 +0900

    Fix: voicediary intro page css animation

--- frontend/voda/src/components/user/AnalysisPet.jsx
+++ frontend/voda/src/components/user/AnalysisPet.jsx
@@ -1,3 +1,80 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f8f2067e076ee78d8c4844f5e96897dd1ca8aa3c589996056efe42656c6f5038
-size 2000
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { usePetStore } from "../../store/petStore";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import { getPet } from "../../services/pet";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 25% auto;
+`;
+
+const AnalysisPet = () => {
+  const EMOJI_URL = import.meta.env.VITE_EMOJI_URL;
+  const { petMap } = usePetStore();
+  const [stage, setStage] = useState();
+  const [petAppearance, setPetAppearance] = useState();
+  const [name, setName] = useState();
+  const [petImageUrl, setPetImageUrl] = useState();
+  const fetchData = async () => {
+    try {
+      const data = await getPet();
+      setStage(data.pet.stage);
+      setPetAppearance(data.pet.petAppearance);
+      setName(data.pet.name);
+      setPetImageUrl(`${petMap[data.pet.stage][data.pet.petAppearance]}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "1.5rem",
+          fontWeight: "bold",
+          marginBottom: "5vh",
+        }}
+      >
+        {name} 얼마나 컸는지 <br />
+        보러 가볼까요?
+      </div>
+      <img
+        src={`${EMOJI_URL}/${petImageUrl}`}
+        style={{
+          width: "15rem",
+          height: "15rem",
+          marginBottom: "5vh",
+        }}
+      />
+      <Button
+        variant="contained"
+        style={{
+          width: "65vw",
+          height: "5vh",
+          marginBottom: "5vh",
+          backgroundColor: "#B4D89E",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "1rem",
+        }}
+        onClick={() => {
+          navigate("/pet");
+        }}
+      >
+        보러 가기
+      </Button>
+    </Container>
+  );
+};
+
+export default AnalysisPet;
