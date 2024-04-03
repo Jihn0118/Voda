101966bac14aff741834908332cb89b4a01a6741
zqwz <zqwz@naver.com>
2024-04-03 11:20:03 +0900

    fix : 일기 통계 페이지 오류 수정

--- backend/src/main/java/io/watssuggang/voda/member/dto/res/EmotionReportResponse.java
+++ backend/src/main/java/io/watssuggang/voda/member/dto/res/EmotionReportResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d13952f2777c350b61082542bc1e7dd4ae71fa88d1100a42f682d32e4a45851
-size 2630
+oid sha256:9868735496875a4b746f74da2de12355fbcff691d7be7aef2bbfe6eabea912d5
+size 2629
